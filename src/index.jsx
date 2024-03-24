778e3b20fcb41d784fb5b63bc85f1864e67c9e8a
Stanislav Shurko <stanislav.shurko@coffeecup.app>
2024-03-24 15:15:21 +0100

    Redux store set + Redux/toolkit installed

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+// Redux
+import { Provider } from 'react-redux';
+import store from './store/store';
 import App from './App';
-import './services/translations/i18n.js';
+import './services/translations/i18n';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
